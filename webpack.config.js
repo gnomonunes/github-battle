585e7bd85552686dd87fd00874e9a76218327392
Fernando Nunes <gnomonunes@gmail.com>
2018-03-26 20:51:09 +0100

    First React component

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './app/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]_bundle.js'
+  },
+  module: {
+    rules: [
+      {test: /\.(js)$/, use: 'babel-loader'},
+      {test: /\.css/, use: ['style-loader', 'css-loader']}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/html/index.html'
+    })
+  ]
+}
