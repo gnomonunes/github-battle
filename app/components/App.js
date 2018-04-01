09f481356b13ba871a4960342bdd02d041a1774c
Fernando Nunes <gnomonunes@gmail.com>
2018-04-01 13:00:35 +0100

    Update styles

--- app/components/App.js
+++ app/components/App.js
@@ -5,7 +5,7 @@ var Popular = require('./Popular');
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className='container'>
         <Popular />
       </div>
     )
