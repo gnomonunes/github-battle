e78e7315fb14123dfeb69d35198b894e420a2829
Fernando Nunes <gnomonunes@gmail.com>
2018-04-01 13:52:20 +0100

    Add language list item click handler to Popular component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,12 +2,33 @@ var React = require('react');
 var PropTypes = require('prop-types');
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All'
+    };
+  }
+
+  updateLanguage(language) {
+    this.setState(() => ({ selectedLanguage: language }));
+  }
+
   render() {
     var languages = ["All", "JavaScript", "Java", "Ruby", "Python", "PHP"];
 
     return (
       <ul className="languages">
-        {languages.map(language => <li key={language}>{language}</li>)}
+        {languages.map(language => {
+          return (
+            <li
+              key={language}
+              onClick={this.updateLanguage.bind(this, language)}
+              style={this.state.selectedLanguage == language ? {color: '#d0021b'} : null}>
+                {language}
+            </li>
+          );
+        })}
       </ul>
     )
   }
