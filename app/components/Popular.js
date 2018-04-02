930a1d7c6da6c27ae638904321ebd46c77fca103
Fernando Nunes <gnomonunes@gmail.com>
2018-04-02 14:21:31 +0100

    Make SelectLanguage a separate component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,30 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 
+const LANGUAGES = ["All", "JavaScript", "Java", "Ruby", "Python", "PHP"];
+
+var SelectLanguage = (props) => {
+  return (
+    <ul className="languages">
+      {LANGUAGES.map(language => {
+        return (
+          <li
+          key={language}
+          onClick={props.onSelect.bind(null, language)}
+          style={props.selectedLanguage == language ? {color: '#d0021b'} : null}>
+          {language}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+}
+
 class Popular extends React.Component {
   constructor(props) {
     super(props);
@@ -8,6 +32,8 @@ class Popular extends React.Component {
     this.state = {
       selectedLanguage: 'All'
     };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
   }
 
   updateLanguage(language) {
@@ -15,21 +41,12 @@ class Popular extends React.Component {
   }
 
   render() {
-    var languages = ["All", "JavaScript", "Java", "Ruby", "Python", "PHP"];
-
     return (
-      <ul className="languages">
-        {languages.map(language => {
-          return (
-            <li
-              key={language}
-              onClick={this.updateLanguage.bind(this, language)}
-              style={this.state.selectedLanguage == language ? {color: '#d0021b'} : null}>
-                {language}
-            </li>
-          );
-        })}
-      </ul>
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage} />
+      </div>
     )
   }
 }
