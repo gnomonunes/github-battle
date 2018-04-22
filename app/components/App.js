eb0a2fef417ec5c4759de62818c0554c0fb82759
Fernando Nunes <gnomonunes@gmail.com>
2018-04-22 12:10:37 +0100

    Add application router

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
+import Navigation from "./Navigation";
 import Popular from "./Popular";
 
 class App extends React.Component {
   render() {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className="container">
+          <Navigation />
+          <Route exact path="/" render={(props) => <h1>Home</h1>} />
+          <Route path="/popular" component={Popular} />
+        </div>
+      </Router>
     )
   }
 }
