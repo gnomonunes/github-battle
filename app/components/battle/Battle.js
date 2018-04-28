af2cf34b2a24fd4c0a37b55a08ff7da21990c9ac
Fernando Nunes <gnomonunes@gmail.com>
2018-04-28 14:06:29 +0100

    Add Player form

--- app/components/battle/Battle.js
+++ app/components/battle/Battle.js
@@ -1,11 +1,47 @@
 import React from "react";
 import Player from "./Player";
+import PlayerForm from "./PlayerForm";
 
-const Battle = (props) => (
-  <div className="row">
-    <Player label="Player One" />
-    <Player label="Player Two" />
-  </div>
-);
+class Battle extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      playersOne: {
+        username: null,
+        image: null
+      },
+      playerTwo: {
+        username: null,
+        image: null
+      }
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(id, username) {
+    const newState = {}
+    newState[id] = {
+      username: username,
+    }
+
+    this.setState(newState);
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <PlayerForm
+            id="playersOne" label="Player One"
+            onSubmit={this.handleSubmit} />
+        <PlayerForm
+            id="playersTwo" label="Player Two"
+            onSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
+}
+
 
 export default Battle;
---
+++ app/components/battle/PlayerForm.js
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class PlayerForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: ''
+    }
+  }
+
+  handleChange(event) {
+    const value = event.target.value;
+
+    this.setState({
+      username: value
+    })
+  }
+
+  handleSumit(event) {
+    event.preventDefault();
+    
+    this.props.onSubmit(this.props.id, this.state.username);
+  }
+
+  render() {
+    return (
+      <form className="column" onSubmit={this.handleSumit.bind(this)}>
+        <label htmlFor="username" className="header">
+          {this.props.label}
+        </label>
+        <input
+          type="text"
+          id="username"
+          placeholder="github username"
+          autoComplete="off"
+          onChange={this.handleChange.bind(this)} />
+        <button className="button" type="submit">submit</button>
+      </form>
+    )
+  }
+}
+
+PlayerForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired
+}
+
+export default PlayerForm;
