ed166b7321468019f40dca33745da797a08bfa89
Fernando Nunes <gnomonunes@gmail.com>
2018-07-17 21:49:04 +0100

    Complete Battle component

--- app/components/battle/Battle.js
+++ app/components/battle/Battle.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PlayerForm from "./PlayerForm";
 import PlayerPreview from "./PlayerPreview";
+import GithubConnector from "../../utils/github-connector";
 
 class Battle extends React.Component {
   constructor(props) {
@@ -9,21 +11,39 @@ class Battle extends React.Component {
     this.state = {
       playerOne: {
         username: null,
-        image: null
+        avatar: null
       },
       playerTwo: {
         username: null,
-        image: null
+        avatar: null
       }
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
 
   handleSubmit(id, username) {
+
+    GithubConnector.fetchUser(username)
+      .then((user) => {
+        const newState = {}
+
+        newState[id] = {
+          username: user.login,
+          avatar: user.avatar_url
+        }
+
+        this.setState(newState);
+      });
+  }
+
+  handleReset(id) {
     const newState = {}
+
     newState[id] = {
-      username: username,
+      username: null,
+      avatar: null
     }
 
     this.setState(newState);
@@ -31,22 +51,42 @@ class Battle extends React.Component {
 
   render() {
     return (
-      <div className="row">
-        {!this.state.playerOne.username ?
-          <PlayerForm
-              id="playerOne" label="Player One"
-              onSubmit={this.handleSubmit} />
-          :
-          <PlayerPreview username={this.state.playerOne.username} />
-        }
-        {!this.state.playerTwo.username ?
-          <PlayerForm
-              id="playerTwo" label="Player Two"
-              onSubmit={this.handleSubmit} />
-          :
-          <PlayerPreview username={this.state.playerTwo.username} />
-        }
-      </div>
+      <div>
+        <div className="row">
+          {!this.state.playerOne.username ?
+            <PlayerForm
+                id="playerOne" label="Player One"
+                onSubmit={this.handleSubmit} />
+            :
+            <PlayerPreview
+              avatar={this.state.playerOne.avatar}
+              username={this.state.playerOne.username}
+              onReset={this.handleReset}
+              id="playerOne" />
+          }
+          {!this.state.playerTwo.username ?
+            <PlayerForm
+                id="playerTwo" label="Player Two"
+                onSubmit={this.handleSubmit} />
+            :
+            <PlayerPreview
+              avatar={this.state.playerTwo.avatar}
+              username={this.state.playerTwo.username}
+              onReset={this.handleReset}
+              id="playerTwo" />
+          }
+        </div>
+
+        {this.state.playerOne.username && this.state.playerTwo.username &&
+          <Link
+            className='button'
+            to={{
+              pathname: `${this.props.match.url}/results`,
+              search: `?playerOneUsername=${this.state.playerOne.username}&playerTwoUsername=${this.state.playerTwo.username}`
+            }}>
+            Battle
+          </Link>}
+        </div>
     );
   }
 }
