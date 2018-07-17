3373997ee458d50577ce1913417d14998b8c90a3
Fernando Nunes <gnomonunes@gmail.com>
2018-07-17 21:45:40 +0100

    Complete PlayerPreview component

--- app/components/battle/PlayerPreview.js
+++ app/components/battle/PlayerPreview.js
@@ -3,8 +3,27 @@ import PropTypes from "prop-types";
 
 const PlayerPreview = (props) => (
   <div>
-    {props.username}
+    <div className='column'>
+      <img
+        className='avatar'
+        src={props.avatar}
+        alt={'Avatar for ' + props.username}
+      />
+      <h2 className='username'>@{props.username}</h2>
+    </div>
+    <button
+      className='reset'
+      onClick={props.onReset.bind(null, props.id)}>
+        Reset
+    </button>
   </div>
 )
+
+PlayerPreview.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired
+}
 
 export default PlayerPreview;
