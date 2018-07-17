22d974ba1cd48b3b6f3585d4dbdf4d3917ac334b
Fernando Nunes <gnomonunes@gmail.com>
2018-07-17 21:50:05 +0100

    Add fetch user info method to Github connector

--- app/utils/github-connector.js
+++ app/utils/github-connector.js
@@ -2,10 +2,16 @@ import axios from "axios";
 
 const GithubConnector = {
   fetchPopularRepos: (language) => {
-    const uri = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
+    const uri = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
 
     return axios.get(uri)
       .then((response) => response.data.items);
+  },
+  fetchUser: (username) => {
+    const uri = window.encodeURI(`https://api.github.com/search/users?q=${username}`);
+
+    return axios.get(uri)
+      .then((response) => response.data.items[0]);
   }
 }
 
