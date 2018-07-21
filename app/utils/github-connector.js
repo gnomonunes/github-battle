4009d1cca760f357fa95fde7dfe751b21af23de3
Fernando Nunes <gnomonunes@gmail.com>
2018-07-21 12:45:54 +0100

    Add user data methods to GithubConnector

--- app/utils/github-connector.js
+++ app/utils/github-connector.js
@@ -1,17 +1,51 @@
 import axios from "axios";
 
-const GithubConnector = {
-  fetchPopularRepos: (language) => {
-    const uri = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
+const apiUrl = 'https://api.github.com';
 
-    return axios.get(uri)
-      .then((response) => response.data.items);
+const userData = (username) => {
+  return axios.all([
+    profile(username),
+    repos(username)
+  ]).then(data => {
+    return {
+      profile: data[0],
+      repos: data[1]
+    }
+  })
+}
+
+const profile = (username) => {
+  return get(`${apiUrl}/users/${username}`)
+    .then(response => response.data);
+}
+
+const repos = (username) => {
+  return get(`${apiUrl}/users/${username}/repos`)
+    .then(response => response.data);
+}
+
+const get = (uri) => {
+  const encodedUri = window.encodeURI(uri);
+
+  return axios.get(uri);
+}
+
+const GithubConnector = {
+  battle: (usernames) => {
+    usernames.map(username => {
+      userData(username)
+        .then((user) => {
+          console.log(user);
+        })
+    })
+  },
+  fetchPopularRepos: (language) => {
+    return get(`${apiUrl}/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
+      .then(response => response.data.items);
   },
   fetchUser: (username) => {
-    const uri = window.encodeURI(`https://api.github.com/search/users?q=${username}`);
-
-    return axios.get(uri)
-      .then((response) => response.data.items[0]);
+    return get(`${apiUrl}/search/users?q=${username}`)
+      .then(response => response.data.items[0]);
   }
 }
 
