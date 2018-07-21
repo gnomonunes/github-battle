23bdf0933b1d7d5537289edfac9f589f18788c25
Fernando Nunes <gnomonunes@gmail.com>
2018-07-21 12:47:10 +0100

    Retrive player Github data

--- app/components/battle/Results.js
+++ app/components/battle/Results.js
@@ -1,6 +1,23 @@
 import React from "react";
+import GithubConnector from "../../utils/github-connector";
+
+// searchString.match(/\?playerOneUsername=(?<playerOneUsername>\w+)&playerTwoUsername=(?<playerTwoUsername>\w+)/).groups['playerOneUsername']
+
+const parseUsernames = (searchString) => {
+  const playersRegex = /\?playerOneUsername=(?<playerOne>\w+)&playerTwoUsername=(?<playerTwo>\w+)/;
+
+  return searchString.match(playersRegex).groups;
+}
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const usernames = Object.values(parseUsernames(props.location.search));
+
+    console.log(GithubConnector.battle(usernames));
+  }
+
   render() {
     return (
       <div>Results</div>
